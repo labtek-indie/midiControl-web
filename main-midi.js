a78d179b6423c52e958f272ecd9904f6ef17563b
Elian Daiva <elian.alien19@gmail.com>
2018-04-24 19:07:29 +0700

    controlling launchpad color on novation

--- main-midi.js
+++ main-midi.js
@@ -1,42 +1,48 @@
-var MidiStream = require('midi-stream')
-var Observ = require('observ')
-var ArrayGrid = require('array-grid')
-
-// var grid = ArrayGrid([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],[4,4])
-// grid.get(0,1)
-// grid.get(1,2)
-// grid.get(2,3)
-// grid.get(3,4)
-// grid.data
+let MidiStream = require('midi-stream')
+let Observ = require('observ')
+let ArrayGrid = require('array-grid')
 
 // use Launchpad for Novation Launchpad
 // use MPKmini2 for Akai MPK Mini2
-var launchpad = MidiStream ('Launchpad',{
+let launchpad = MidiStream ('Launchpad',{
 	normalizeNotes: true
 })
-launchpad.on('data', console.log)
-launchpad.on('data', (data) => launchpad.write(data))
-
-// make parse a grid value
-function LaunchpadToGrid(midiPort){
-	var obs = Observ(ArrayGrid([],[8,8]))
-	midiPort.on('data',(data) => {
-		var col = data[1] % 16 
-		var row = Math.floor(data[1] / 16)
-		if (col < 8 && row < 8) {
-			var newValue = ArrayGrid(obs().data.slice(), obs().shape)
-			newValue.set(row, col, data[2])
-			obs.set(newValue)
+launchpad.on('data', (data) => console.log(data))
+launchpad.on('data', (data) => {
+	if(data[1] == 104){
+		for (let i=0; i < 120; i++){
+			launchpad.write([144, i, 0])
+			// console.log('normalize')
 		}
-	})
-	return obs
-}
+	} 
+	else {
+		let x =  Math.floor(Math.random()*127)
+		launchpad.write([data[0], data[1], x])
+		// console.log("color ",x)
+	}
+		
+})
+	
 
-// input grid container 
-var inputGrid = LaunchpadToGrid(launchpad)
+// // make parse a grid value
+// function LaunchpadToGrid(midiPort){
+// 	var obs = Observ(ArrayGrid([],[8,8]));
+// 	midiPort.on('data',(data) => {
+// 		var col = data[1] % 16 
+// 		var row = Math.floor(data[1] / 16)
+// 		if (col < 8 && row < 8) {
+// 			var newValue = ArrayGrid(obs().data.slice(), obs().shape)
+// 			newValue.set(row, col, data[2])
+// 			obs.set(newValue)
+// 		}
+// 	})
+// 	return obs
+// }
 
-// cara menulis function JS 1
-// function drawTerminal(grid) {
+// // input grid container 
+// var inputGrid = LaunchpadToGrid(launchpad)
+
+// inputGrid((grid) => {
 // 	var result = ''
 // 	for (var row = 0; row < grid.shape[0]; row++) {
 // 		for (var col = 0; col < grid.shape[1]; col++) {
@@ -44,18 +50,6 @@ var inputGrid = LaunchpadToGrid(launchpad)
 // 		}
 // 		result += '\n'
 // 	}
-// 	console.log(result)
-// }
-// inputGrid(drawTerminal)
+// 	console.log(result)	
+// })
 
-// cara menulis function JS 2
-inputGrid((grid) => {
-	var result = ''
-	for (var row = 0; row < grid.shape[0]; row++) {
-		for (var col = 0; col < grid.shape[1]; col++) {
-			result += (' ' + (grid.get(row,col) || 0)).slice(-3) + ' '
-		}
-		result += '\n'
-	}
-	console.log(result)	
-})
